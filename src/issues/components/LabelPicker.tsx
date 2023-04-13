1c132eba1dfc36b00722b72bf9693705ab8d1242
Joao Castillo <joaocastillo91@gmail.com>
2023-04-13 16:03:52 -0500

    commit 13/04/2023

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,13 +1,38 @@
+import { FC } from "react";
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { useLabels } from "../hooks/useLabels";
 
-export const LabelPicker = () => {
+
+interface Props{
+  selectedLabels:string[];
+  onChange:( labelName: string )=>void;
+}
+
+export const LabelPicker:FC<Props> = ({
+  selectedLabels,
+  onChange  
+}) => {
+
+const {labelsQuery} = useLabels();
+
+if( labelsQuery.isLoading )
+return ( <LoadingIcon/> );
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
+      {
+        labelsQuery.data?.map( label=>(
+          <span 
+            key={ label.id }
+            className={`badge rounded-pill m-1 label-picker ${selectedLabels.includes(label.name) ? 'label-active' : ''}`}
+            style={{ border: `1px solid #${ label.color}`, color: `#${label.color}` }}
+            onClick={()=> onChange(label.name)}
+          >
+            { label.name }
+          </span>
+        ))
+      }
+
         
     </div>
   )
